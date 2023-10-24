bc61aaa73d5e7e5b8ba7aacd72527067ad0d14bf
Riccardo Spadon <spadon.riccardo@gmail.com>
2023-10-24 20:54:43 +0200

    EPIBOOKS
    - Completato l'esercizio 3 ed l'esercizio EXTRA;

--- src/AllTheBooks.jsx
+++ src/AllTheBooks.jsx
@@ -1,9 +1,37 @@
+import { useState } from "react";
+import { Container, Row, Form, Col } from "react-bootstrap";
+import fantasy from "./books/fantasy.json" // DA CAMBIARE CON I FILE .json CHE SONO PRESENTI NELLA CARTELLA 'books'
 import SingleBook from "./SingleBook";
 
 export default function AllTheBooks() {
+    const [searchQuery, setSearchQuery] = useState('')
+
     return (
         <>
-            <SingleBook />
+            <Container className="my-5">
+                <Row className="my-5">
+                    <Form.Group>
+                        <Form.Control
+                            type="search"
+                            placeholder="Search..."
+                            value={searchQuery}
+                            onChange={(event) => setSearchQuery(event.target.value)}
+                        />
+                    </Form.Group>
+                </Row>
+
+                <Row>
+                    {fantasy
+                        .filter((b) => b.title.toLowerCase().includes(searchQuery))
+                        .map((book) => {
+                            return (
+                                <Col xs={6} md={3} key={book.asin}>
+                                    <SingleBook book={book} />
+                                </Col>
+                            )
+                        })}
+                </Row>
+            </Container>
         </>
     );
 }
--- src/SingleBook.jsx
+++ src/SingleBook.jsx
@@ -1,27 +1,23 @@
-import Container from 'react-bootstrap/Container';
-import Row from 'react-bootstrap/Row';
-import Col from 'react-bootstrap/Col';
 import Card from 'react-bootstrap/Card';
+import { useState } from 'react';
 
-export default function SingleBook() {
-    let books = require('./books/fantasy.json') //DA CAMBIARE CON I FILE .json CHE SONO PRESENTI NELLA CARTELLA 'books'
+export default function SingleBook({ book }) {
+   
+    const [selected, setSelected] = useState(false)
 
-    return(
-        <Container className = 'my-5' >
-            <Row>
-                {books.map((book) => (
-                    <Col xs={6} md={3} key={book.asin}>
-                        <Card className="mb-5" id={`book_${book.asin}`}>
-                            <Card.Img src={book.img} variant="top" className='image' />
-                            <Card.Body class="card-body p-1">
-                                <h5 className="card-title">{book.title}</h5>
-                                <p className="card-text">Categoria: {book.category}</p>
-                                <p className="card-text"> Prezzo: {book.price}</p>
-                            </Card.Body>
-                        </Card>
-                    </Col>
-                ))}
-            </Row>
-        </Container >
+    return (
+        <>
+            <Card onClick={() => setSelected(!selected)}
+                style={{ border: selected ? '2px solid red' : 'none' }}
+                className="mb-5" id={`book_${book.asin}`}
+                >
+                <Card.Img src={book.img} variant="top" className='image' />
+                <Card.Body class="card-body p-1">
+                    <h5 className="card-title">{book.title}</h5>
+                    <p className="card-text">Categoria: {book.category}</p>
+                    <p className="card-text"> Prezzo: {book.price}</p>
+                </Card.Body>
+            </Card>
+        </>
     );
 }
