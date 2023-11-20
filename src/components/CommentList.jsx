b78b462de92c3d0e12f41bafdee42e10966317a0
Riccardo Spadon <spadon.riccardo@gmail.com>
2023-11-20 19:02:51 +0100

    EPIBOOKS
    - Aggiunto un"return" in CommentList che mancava;
    - Eliminato il tasto della modifica di un commento;

--- src/components/CommentList.jsx
+++ src/components/CommentList.jsx
@@ -1,10 +1,12 @@
 import { ListGroup } from 'react-bootstrap'
 import SingleComment from './SingleComment'
 
-export default function CommentList ({ commentsToShow }) {
-  <ListGroup style={{ color: 'black' }} className="mt-2 mb-5">
-    {commentsToShow.map((comment) => (
-      <SingleComment comment={comment} key={comment._id} />
-    ))}
-  </ListGroup>
+export default function CommentList({ commentsToShow }) {
+  return (
+    <ListGroup style={{ color: 'black' }} className="mt-2 mb-5">
+      {commentsToShow.map((comment) => (
+        <SingleComment comment={comment} key={comment._id} />
+      ))}
+    </ListGroup>
+  )
 }
--- src/components/SingleComment.jsx
+++ src/components/SingleComment.jsx
@@ -1,85 +1,52 @@
 import { Button, ListGroup, } from "react-bootstrap";
-import { Trash, PencilSquare } from "react-bootstrap-icons";
+import { Trash } from "react-bootstrap-icons";
 import { useCallback } from "react";
 
 export default function SingleComment({ setComments, comment, id }) {
-    
-    const getComments = useCallback(() => {
-        fetch(`https://striveschool-api.herokuapp.com/api/books/${id}/comments/`,
-        {
-          method: 'GET',
-          headers: {
-            Authorization: 'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI2NTM3ZjhiMDc3Y2RhYTAwMTQ2ZGYzODEiLCJpYXQiOjE3MDAzMzY1NDEsImV4cCI6MTcwMTU0NjE0MX0.K4MioEDi6vNxOFIwjRjACyIevOSwDmYUcIsG_PSScQ4',
-          },
-        })
-          .then((response) => response.json())
-          .then(setComments)
-      });
-    
-    
-    const deleteComment = (asin) => {
-        fetch('https://striveschool-api.herokuapp.com/api/comments/' + asin,
-            {
-                method: 'DELETE',
-                headers: {
-                    Authorization: 'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI2NTM3ZjhiMDc3Y2RhYTAwMTQ2ZGYzODEiLCJpYXQiOjE3MDAzMzY1NDEsImV4cCI6MTcwMTU0NjE0MX0.K4MioEDi6vNxOFIwjRjACyIevOSwDmYUcIsG_PSScQ4',
-                },
-            }).then((response) => {
-                if (response.ok) {
-                    alert('Your review was deleted successfully!')
-                } else {
-                    alert('Your review was not deleted!')
-                }
-            })
-            .then(getComments())
-            .catch(error => console.error(error))
-    }
 
-    const changeComment = (asin) => {
-        fetch(
-            'https://striveschool-api.herokuapp.com/api/comments/' + asin,
-            {
-              method: 'PUT',
-              headers: {
-                Authorization: 'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI2NTM3ZjhiMDc3Y2RhYTAwMTQ2ZGYzODEiLCJpYXQiOjE3MDAzMzY1NDEsImV4cCI6MTcwMTU0NjE0MX0.K4MioEDi6vNxOFIwjRjACyIevOSwDmYUcIsG_PSScQ4',
-              },
-              body:JSON.stringify({ comment:'nuovo commento', rate:5 })
-            }
-          )
-          .then((response) => {
-          if (response.ok) {
-            alert('The review was successful modified');
-            
-          } else {
-            throw new Error('The review was not successful modified')
-          }
-          
-        })
-        .then(getComments())
-        .catch((error) => console.error(error));
-      }
+  const getComments = useCallback(() => {
+    fetch(`https://striveschool-api.herokuapp.com/api/books/:${id}/comments/`,
+      {
+        method: 'GET',
+        headers: {
+          Authorization: 'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI2NTM3ZjhiMDc3Y2RhYTAwMTQ2ZGYzODEiLCJpYXQiOjE3MDAzMzY1NDEsImV4cCI6MTcwMTU0NjE0MX0.K4MioEDi6vNxOFIwjRjACyIevOSwDmYUcIsG_PSScQ4',
+        },
+      })
+      .then((response) => response.json())
+      .then(setComments)
+  });
 
-    return (
-        <ListGroup.Item className="d-flex justify-content-between">
-        <h6>{comment.comment} - {comment.rate}</h6>
-        <div>
-        <Button
-          variant="warning"
-          className="ms-2 rounded-circle" 
-          onClick={() => changeComment(comment._id)} 
-        >
-          <PencilSquare />
-        </Button>
-  
-  
+
+  const deleteComment = (asin) => {
+    fetch('https://striveschool-api.herokuapp.com/api/comments/' + asin,
+      {
+        method: 'DELETE',
+        headers: {
+          Authorization: 'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI2NTM3ZjhiMDc3Y2RhYTAwMTQ2ZGYzODEiLCJpYXQiOjE3MDAzMzY1NDEsImV4cCI6MTcwMTU0NjE0MX0.K4MioEDi6vNxOFIwjRjACyIevOSwDmYUcIsG_PSScQ4',
+        },
+      }).then((response) => {
+        if (response.ok) {
+          alert('Your review was deleted successfully!')
+        } else {
+          alert('Your review was not deleted!')
+        }
+      })
+      .then(getComments())
+      .catch(error => console.error(error))
+  }
+
+  return (
+    <ListGroup.Item className="d-flex justify-content-between">
+      <h6>{comment.comment} - {comment.rate}</h6>
+      <div>
         <Button
           variant="danger"
-          className="ms-2 rounded-circle" 
+          className="ms-2 rounded-circle"
           onClick={() => deleteComment(comment._id)}
         >
           <Trash />
         </Button>
-        </div>
-      </ListGroup.Item>
-    )
+      </div>
+    </ListGroup.Item>
+  )
 }
